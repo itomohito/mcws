ebd122fc3bd3874d95574c5c8ffdb38b08cc6b69
itomohito <97725445+itomohito@users.noreply.github.com>
2022-04-25 13:09:33 +0600

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<!--footer-->
+    <div class="footer">
+       <p>All rights Reserved 2021 &copy; "Salute" medical center </p>
+    </div>
+        <!--//footer-->  
+		
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,73 @@
+  <div class=" sidebar" role="navigation">
+            <div class="navbar-collapse">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+          <ul class="nav" id="side-menu">
+            
+            <li>
+              <a href="add-services.php"><i class="fa fa-cogs nav_icon"></i>Services<span class="fa arrow"></span> </a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="add-services.php">Add services</a>
+                </li>
+                <li>
+                  <a href="manage-services.php">Edit services</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+            <li class="">
+              <a href="about-us.php"><i class="fa fa-book nav_icon"></i>Pages <span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="about-us.php">About us</a>
+                </li>
+                <li>
+                  <a href="contact-us.php">Contact us</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+          
+            <li>
+              <a href="all-appointment.php"><i class="fa fa-check-square-o nav_icon"></i>Appointment<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="all-appointment.php">All appointments</a>
+                </li>
+                <li>
+                  <a href="new-appointment.php">New appointments</a>
+                </li>
+                <li>
+                  <a href="accepted-appointment.php">Accepted appointments</a>
+                </li>
+                <li>
+                  <a href="rejected-appointment.php">Rejected appointments</a>
+                </li>
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+           
+        
+            <li>
+              <a href="add-customer.php" class="chart-nav"><i class="fa fa-user nav_icon"></i>Add clients</a>
+            </li>
+             <li>
+              <a href="customer-list.php" class="chart-nav"><i class="fa fa-users nav_icon"></i>List of clients</a>
+            </li>
+              
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+
+ 
+            <li>
+              <a href="search-appointment.php" class="chart-nav"><i class="fa fa-search nav_icon"></i>Search for appointment</a>
+            </li>
+
+
+          </ul>
+          <div class="clearfix"> </div>
+          <!-- //sidebar-collapse -->
+        </nav>
+      </div>
+    </div>
